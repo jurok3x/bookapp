aa8413dfcc8dc1feed8ab7a0b3c09c0d0513b7e2
ykotsiuba <jurok3x@gmail.com>
2024-08-13 15:44:10 +0300

    Added dtos .

---
+++ src/main/java/com/ykotsiuba/bookapp/dto/BookSaveRequestDTO.java
@@ -0,0 +1,9 @@
+package com.ykotsiuba.bookapp.dto;
+
+import lombok.Data;
+
+@Data
+public class BookSaveRequestDTO {
+    private String title;
+    private String author;
+}
---
+++ src/main/java/com/ykotsiuba/bookapp/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.ykotsiuba.bookapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+public class MemberDTO {
+    private Long id;
+    private String name;
+    private LocalDate membershipDate;
+    @JsonProperty("books")
+    private Set<BookDTO> booksDTO;
+}
